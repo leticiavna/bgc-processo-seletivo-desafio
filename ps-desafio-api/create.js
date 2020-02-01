42f81a8396a9fafdef28ede573c3a14e8b0559f0
Leticia Vidal <leticiavna@id.uff.br>
2020-02-01 18:14:23 +0000

    fix: update dynamodb region

--- ps-desafio-api/create.js
+++ ps-desafio-api/create.js
@@ -5,10 +5,11 @@ import { success, failure } from "./libs/response-lib";
 export async function main(event, context) {
   const data = JSON.parse(event.body);
   const params = {
+    // env var
     TableName: process.env.tableName,
     Item: {
       userId: event.requestContext.identity.cognitoIdentityId,
-      noteId: uuid.v1(),
+      purchaseId: uuid.v1(),
       content: data.content,
       attachment: data.attachment,
       createdAt: Date.now()
@@ -19,6 +20,7 @@ export async function main(event, context) {
     await dynamoDbLib.call("put", params);
     return success(params.Item);
   } catch (e) {
+    console.log(e);
     return failure({ status: false });
   }
 }
--- ps-desafio-api/libs/dynamodb-lib.js
+++ ps-desafio-api/libs/dynamodb-lib.js
@@ -1,6 +1,7 @@
 import AWS from "aws-sdk";
 
 export function call(action, params) {
+  AWS.config.update({ region: "us-east-2" });
   const dynamoDb = new AWS.DynamoDB.DocumentClient();
   // usa promise para lidar com assincrono
   return dynamoDb[action](params).promise();
