8ab178b8a9998a94e21a12cc712ec16bac55eb51
Leticia Vidal <leticiavna@id.uff.br>
2020-02-01 21:49:18 +0000

    feat: add aws amplify and configs

---
+++ ps-desafio-front/src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "uploads-minions-bgc"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://daavhc9qq7.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_P0GDzx4ku",
+    APP_CLIENT_ID: "2grbssh95iktfklbuj663tcc05",
+    IDENTITY_POOL_ID: "us-east-2:a23e81b0-a049-4751-ae8a-2e8846324563"
+  }
+};
---
+++ ps-desafio-front/src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Lorem Ipsum 2</h1>
+        <p>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."</p>
+      </div>
+    </div>
+  );
+}
