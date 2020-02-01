d606ccf65cb8e12fcba08835b7886058dc1144fc
Leticia Vidal <leticiavna@id.uff.br>
2020-02-01 05:06:55 +0000

    feat: add serverless.yml configurations and first function (create)

---
+++ ps-desafio-api/create.js
@@ -0,0 +1,45 @@
+import uuid from "uuid"; // gera IDs unicos
+import AWS from "aws-sdk"; // comunica com os outros servicos
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+export function main(event, context, callback) {
+  const data = JSON.parse(event.body);
+
+  const params = {
+    TableName: process.env.tableName, // environment variable
+    Item: { // contains the attributes of the item to be created
+      userId: event.requestContext.identity.cognitoIdentityId, // cognito
+      purchaseId: uuid.v1(),
+      content: data.content, //parsed from request body
+      attachment: data.attachment, // parsed from request body
+      createdAt: Date.now() // 
+    }
+  };
+
+  dynamoDb.put(params, (error, data) => {
+    const headers = {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true
+    };
+
+    // Return status code 500 on error
+    if (error) {
+      const response = {
+        statusCode: 500,
+        headers: headers,
+        body: JSON.stringify({ status: false })
+      };
+      callback(null, response);
+      return;
+    }
+
+    // Return status code 200 and the newly created item
+    const response = {
+      statusCode: 200,
+      headers: headers,
+      body: JSON.stringify(params.Item)
+    };
+    callback(null, response);
+  });
+}
