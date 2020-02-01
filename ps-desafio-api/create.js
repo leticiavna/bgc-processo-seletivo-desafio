3a2c4b2794ed8fddbef4a5b08e314eb6cdba2d64
Leticia Vidal <leticiavna@id.uff.br>
2020-02-01 17:38:24 +0000

    fix: add async to function fixing parsing error

--- ps-desafio-api/create.js
+++ ps-desafio-api/create.js
@@ -2,26 +2,23 @@ import uuid from "uuid";
 import * as dynamoDbLib from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
 
-export function main(event, context, callback) {
+export async function main(event, context) {
   const data = JSON.parse(event.body);
-
   const params = {
-    // environment variable
-    TableName: process.env.tableName, 
-    Item: { 
+    TableName: process.env.tableName,
+    Item: {
       userId: event.requestContext.identity.cognitoIdentityId,
-      purchaseId: uuid.v1(),
-      content: data.content, 
+      noteId: uuid.v1(),
+      content: data.content,
       attachment: data.attachment,
       createdAt: Date.now()
     }
   };
-  
+
   try {
     await dynamoDbLib.call("put", params);
     return success(params.Item);
   } catch (e) {
-    console.log(e);
     return failure({ status: false });
   }
 }
