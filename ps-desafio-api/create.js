11fba75750246d163bc134f820668f52435a1df5
Leticia Vidal <leticiavna@id.uff.br>
2020-02-01 15:26:59 +0000

    refactor: add libs folder

--- ps-desafio-api/create.js
+++ ps-desafio-api/create.js
@@ -1,45 +1,27 @@
-import uuid from "uuid"; // gera IDs unicos
-import AWS from "aws-sdk"; // comunica com os outros servicos
-
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
+import uuid from "uuid";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
 
 export function main(event, context, callback) {
   const data = JSON.parse(event.body);
 
   const params = {
-    TableName: process.env.tableName, // environment variable
-    Item: { // contains the attributes of the item to be created
-      userId: event.requestContext.identity.cognitoIdentityId, // cognito
+    // environment variable
+    TableName: process.env.tableName, 
+    Item: { 
+      userId: event.requestContext.identity.cognitoIdentityId,
       purchaseId: uuid.v1(),
-      content: data.content, //parsed from request body
-      attachment: data.attachment, // parsed from request body
+      content: data.content, 
+      attachment: data.attachment,
       createdAt: Date.now()
     }
   };
-
-  dynamoDb.put(params, (error, data) => {
-    const headers = {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": true
-    };
-
-    // Return status code 500 on error
-    if (error) {
-      const response = {
-        statusCode: 500,
-        headers: headers,
-        body: JSON.stringify({ status: false })
-      };
-      callback(null, response);
-      return;
-    }
-
-    // Return status code 200 and the newly created item
-    const response = {
-      statusCode: 200,
-      headers: headers,
-      body: JSON.stringify(params.Item)
-    };
-    callback(null, response);
-  });
+  
+  try {
+    await dynamoDbLib.call("put", params);
+    return success(params.Item);
+  } catch (e) {
+    console.log(e);
+    return failure({ status: false });
+  }
 }
