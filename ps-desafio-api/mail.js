65c10fb02d3540628166c5712daa1cd5932d5d2a
Leticia Vidal <leticiavna@id.uff.br>
2020-02-02 15:37:53 +0000

    fix: trailing spaces and semicolons

--- ps-desafio-api/mail.js
+++ ps-desafio-api/mail.js
@@ -8,16 +8,16 @@ const FROM_MAIL = process.env.FROM_MAIL;
 const TO_MAIL = process.env.TO_MAIL;
 const DOMAIN = process.env.DOMAIN;
 
-// o que tem que ter: 
+// o que tem que ter:
 // 1. funçao pra ser exportada do lambda
 // 2. funcao para passar os parametros do formulario frontend
 
 // Funcao pra passar o formulario
 function generateEmailParams (body) {
-  const { clientMail, clientName, content } = JSON.parse(body)
-  console.log(clientMail, clientName, content)
+  const { clientMail, clientName, content } = JSON.parse(body);
+  console.log(clientMail, clientName, content);
   if (!(clientMail && clientName && content)) {
-    throw new Error('Parâmetros faltando! Confira os parâmetros \'clientMail\', \'clientName\', \'content\'.')
+    throw new Error('Parâmetros faltando! Confira os parâmetros \'clientMail\', \'clientName\', \'content\'.');
   }
 
 return {
@@ -28,7 +28,7 @@ return {
             Body: {
                 Text: {
                 Charset: 'UTF-8',
-                Data: `Mensagem enviada de email ${clientMail} por ${clientName} \Conteúdo: ${content}`
+                Data: `Mensagem enviada de email ${clientMail} por ${clientName}. \Conteúdo: ${content}`
                 }
             },
             Subject: {
@@ -36,15 +36,15 @@ return {
                 Data: `Nova reserva de ${DOMAIN}!`
             }
         }
-    }
+    };
 }
-    
+
 module.exports.mail = async (event) => {
   try {
-    const emailParams = generateEmailParams(event.body)
-    const data = await ses.sendEmail(emailParams).promise()
-    return success(200, data)
+    const emailParams = generateEmailParams(event.body);
+    const data = await ses.sendEmail(emailParams).promise();
+    return success(200, data);
   } catch (err) {
-    return failure(500, err)
+    return failure(500, err);
   }
 }
