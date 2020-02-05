cbde5c645c330bd9b04d46f76be354d07397e10c
Leticia Vidal <leticiavna@id.uff.br>
2020-02-05 04:03:08 +0000

    WIP:fix: trying to fix the reservation form but no success

--- ps-desafio-front/src/containers/Home.js
+++ ps-desafio-front/src/containers/Home.js
@@ -1,24 +1,25 @@
 import React from "react";
 import "./Home.css";
+import { useFormFields } from "../libs/hooksLib";
+import { API } from "aws-amplify"; // faz as chamadas da API
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import { Checkbox, Container, FormControl, Input, InputLabel, ListItemText, MenuItem, Select, TextField } from '@material-ui/core';
 
-// For later
-// eslint-disable-next-line
 const minionsNames = [
-  'stuart',
-  'kevin',
-  'bob',
-  'dave',
-  'phil',
-  'jerry',
+  'Stuart',
+  'Kevin',
+  'Bob',
+  'Dave',
+  'Phil',
+  'Jerry',
 ];
+
 
 // Aqui pode usar a função pra alterar o css
 // Pra não ficar uma coisa enorme, seria legal cada section ter seu arquivo direitinho
-// E  virar um componente, q aí podia chamar na home, fica como TODO
+// E virar um componente, q aí podia chamar na home, fica como TODO
 const useStyles = makeStyles(theme => ({
   lander: {
     backgroundColor: "#0A75BC",
@@ -51,7 +52,7 @@ const useStyles = makeStyles(theme => ({
     padding: theme.spacing(2),
     textAlign: 'center',
     color: theme.palette.text.secondary,
-    transition: "0.3s",
+    transition: "0.4s",
     '&:hover': {
       backgroundColor: "#F4F4F4",
       opacity: 1
@@ -71,6 +72,7 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
+// Material UI properties
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
 const MenuProps = {
@@ -85,15 +87,53 @@ const MenuProps = {
 export default function Home() {
   const classes = useStyles();
   
+  // hook: keeps input data
+  const [fields, handleFieldChange] = useFormFields({
+    name: "",
+    email: ""
+  });
+  
+  // UseState é um React Hook q deixa usar state sem estar numa classe
+  // from docs: useState returns a pair — an array with two items. 
+  // The first item is the current value, and the second is a function that lets us update it.
   const [chosenMinion, setChosenMinion] = React.useState([]);
   
   const handleChange = event => {
     setChosenMinion(event.target.value);
   };
+
+  function validateForm() {
+    return fields.name.length > 0 && fields.email.length > 0 && chosenMinion.length > 0;
+  }
+  
+  async function sendMailPurchase(body) {
+    await API.post("purchases", "/mail", { body: {
+       clientMail: body.email,
+       clientName: body.name,
+       content: body.minions
+    }});
+  }
+  
+  async function handleSubmit(event) {
+  event.preventDefault(); // ????
+  const body = {
+    name: fields.name,
+    email: fields.email,
+    minions: chosenMinion
+  }
+  // chama a api 
+    await API.post("purchases", "/purchases", { body: body
+    }).then(response => {
+    console.log(response);
+    sendMailPurchase(body);
+    alert("minion purchased!");
+    }).catch(error => {
+    alert(error.response)
+    });
+  }
   
   return (
     <div className="Home container">
-      
       <section className={classes.lander} id="lander">
         <Container>
           <h1>sempre quis ter um <b>minion</b>?</h1>
@@ -161,21 +201,22 @@ export default function Home() {
       <section id="purchase">
         <Container className={classes.container}>
           <h1> faça já sua reserva. </h1>
-          <form className={classes.root} noValidate autoComplete="off">
           <div className={classes.root}>
+          <form autoComplete="off" onSubmit={handleSubmit}>
             <Grid container spacing={3}>
               <Grid item xs={6}>
-                <TextField id="name" label="seu nome" fullWidth />
+                <TextField id="name" label="seu nome" value={fields.name} onChange={handleFieldChange} fullWidth />
               </Grid>
               <Grid item xs={6}>
-                <TextField id="email" label="seu email" type="email" fullWidth />
+                <TextField id="email" label="seu email" type="email" value={fields.email} onChange={handleFieldChange} fullWidth />
               </Grid>
               <Grid item xs={12}>
-                <FormControl fullWidth className={classes.formControl}>
-        <InputLabel id="demo-mutiple-checkbox-label">minions que eu quero:</InputLabel>
+                <FormControl className={classes.formControl}>
+        <InputLabel id="minions-label">minions que eu quero:</InputLabel>
         <Select
-          labelId="demo-mutiple-checkbox-label"
-          id="demo-mutiple-checkbox"
+          labelId="minions-label"
+          id="minions"
+          autoWidth
           multiple
           value={chosenMinion}
           onChange={handleChange}
@@ -193,9 +234,10 @@ export default function Home() {
       </FormControl>
               </Grid>
             </Grid>
-            </div>
-            <Button className={classes.purchaseSubmit} fullWidth variant="contained" type="submit">reservar</Button>
+            <Button disabled={!validateForm()} className={classes.purchaseSubmit} variant="contained" type="submit">reservar</Button>
           </form>
+          
+            </div>
         </Container>
       </section>
     </div>
