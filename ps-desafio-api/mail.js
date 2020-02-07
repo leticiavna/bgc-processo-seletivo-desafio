9894c1f1524438caeb3af62511c0b0304966bb6d
Leticia Vidal <leticiavna@id.uff.br>
2020-02-07 06:44:16 +0000

    fix: mail reply_to address and env vars

--- ps-desafio-api/mail.js
+++ ps-desafio-api/mail.js
@@ -5,6 +5,7 @@ var aws = require('aws-sdk');
 var ses = new aws.SES({region: 'us-east-1'});
 const FROM_MAIL = process.env.EMAIL;
 const CC_MAIL = process.env.CC_EMAIL;
+const TO_MAIL = process.env.TO_EMAIL;
 
 // Funcao pra passar o formulario
 function generateEmailParams (body) {
@@ -12,21 +13,21 @@ function generateEmailParams (body) {
   if (!(clientMail && clientName && content)) {
     throw new Error('Parâmetros faltando! Confira os parâmetros \'Email\', \'Nome\' e \'Minions escolhidos\'.');
   }
+
   return {
     Source: FROM_MAIL,
-    Destination: {
-     CcAddresses: [CC_MAIL],
-     ToAddresses: [FROM_MAIL] // FIXME Aqui não vai o email do cliente porque o SES está no modo Sandbox
+    Destination: { 
+      ToAddresses: [TO_MAIL], // FIXME Aqui não vai o email do cliente porque o SES está no modo Sandbox
+      CcAddresses: [CC_MAIL]
+    },
     // O correto seria enviar o email do formulario enviado em ToAddresses
-   },
-    ReplyToAddresses: FROM_MAIL,
+    ReplyToAddresses: [clientMail],
+    // como esta no modo sandbox, o reply_to está com o email do cliente e o ToAddresses esta com o email q era o CC
     Message: {
         Body: {
             Text: {
             Charset: 'UTF-8',
-            Data: `Olá! \n
-            Foi feita uma nova reserva por ${clientName}, com o email ${clientMail}. \n
-            Minions reservados: ${content}`
+            Data: `Olá! \n Foi feita uma nova reserva por ${clientName}, com o email ${clientMail}. \n Minions reservados: ${content}`
             }
         },
         Subject: {
